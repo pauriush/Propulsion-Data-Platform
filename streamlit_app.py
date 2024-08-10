e65fc24ca806a5391bb9c9c10920ed0d880d1a32
Pau <45898358+pauriush@users.noreply.github.com>
2024-08-10 16:27:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-introduction = st.Page("documentation/introduction.py", title="Introduction")
+introduction = st.Page("documentation/introduction.py", title="Introduction", default=True)
 features = st.Page("documentation/features.py", title="Features")
 tutorial_1 = st.Page("tutorials/tutorial-1.py", title="Tutorial 1")
 make_vs_buy = st.Page("resources/make-vs-buy.py", title="Make vs Buy")
