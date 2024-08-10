394c5221215707aa552d72d85d351f9b9d3b0cd2
Pau <45898358+pauriush@users.noreply.github.com>
2024-08-10 16:21:05 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,18 @@
 import streamlit as st
 
+introduction = st.Page("documentation/introduction.py", title="Introduction")
+features = st.Page("documentation/features.py", title="Features")
+tutorial_1 = st.Page("tutorials/tutorial-1.py", title="Tutorial 1")
+make_vs_buy = st.Page("resources/make-vs-buy.py", title="Make vs Buy")
+
+pg = st.navigation(
+    {
+        "Documentation": [introduction, features],
+        "Tutorials": [tutorial_1],
+        "Resources": [make_vs_buy],
+    }
+)
+
 st.title("Propulsion Data Platform")
 st.write(
     "Contents:"
